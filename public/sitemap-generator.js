43101dfa4173ec20b6cc3850f94dc4e470533773
Umar Akhtar <akhtarumar11223@gmail.com>
2025-02-12 10:39:33 +0500

    Updated feature X or fixed bug Y

--- public/sitemap-generator.js
+++ public/sitemap-generator.js
@@ -2,4 +2,4 @@ const Sitemap = require('react-router-sitemap').default;
 const router = require('./src/App').default; // Import your main router
 new Sitemap(router)
   .build('https://abroad-mentors-form.netlify.app/') // Replace with your actual website URL
-  .save('./public/sitemap.xml'); // Save the sitemap.xml file inside the public folder
+  .save('/sitemap.xml'); // Save the sitemap.xml file inside the public folder
